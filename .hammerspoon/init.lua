da41e7b86e21a0c71022dad91f2c0d94a0958b4a
Patrick Wyatt <pat@codeofhonor.com>
2026-03-14 16:22:20 -0700

    Update hammerspoon config

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -2,14 +2,28 @@
 print("--------------------")
 
 -- Load configuration
--- local config = require('personal.config').load()
+local config = require('personal.config').load()
 
 -- Initialize modules
 require('personal.vim-mode').init()
-require('personal.app-launcher').init()
 require('personal.window-border').init()
 require('personal.dim-background-windows').init()
+require('personal.text-expander').init(config.textExpander)
+-- require('personal.app-launcher').init()
 -- require('personal.defeat-paste-blocking').init()
 -- require('personal.keep-computer-awake').init()
 -- require('personal.wifi-watcher').init(config.wifi)
 
+
+-- https://news.ycombinator.com/item?id=47368116
+-- Set window size based on the next two keys pressed
+hs.hotkey.bind({"option", "ctrl"}, "D", function()
+    hs.grid.ui.textSize = 40
+    hs.grid.setGrid('10x3')
+    hs.grid.show()
+end)
+
+hs.hotkey.bind({'cmd'}, 'M', function()
+  -- Do nothing (overrides default minimize)
+  hs.alert.show("Hammerspoon ignoring minimize", 1)
+end)
